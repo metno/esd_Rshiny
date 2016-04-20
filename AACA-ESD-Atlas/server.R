c5217cbcc3038b481e3de0d49956f9b57bd3c02f
brasmus <rasmus.benestad@gmail.com>
2016-04-20 23:23:51 +0200

    update

--- AACA-ESD-Atlas/server.R
+++ AACA-ESD-Atlas/server.R
@@ -1,15 +1,14 @@
 library(shiny)
 library(esd)
-load("dse.aaca.t2m.rcp45.djf.rda")
-Y <- as.station(dse.aaca.t2m.rcp45)
-
+if (!file.exists('dse.eof.t2m.rcp45.rda')) {
+  load("dse.aaca.t2m.rcp45.djf.rda")
+  Z <- as.eof.dsensemble.pca(dse.aaca.t2m.rcp45,verbose=TRUE)
+  save(file='dse.eof.t2m.rcp45.rda',Z)
+} else load('dse.eof.t2m.rcp45.rda')
 shinyServer(function(input, output) {
   
-  function(input, output) {
-    
-    output$map <- renderPlot({ 
-      plot(rnorm(10),main=paste("You have selected", input$Element))
+   output$map <- renderPlot({ 
+    plot(Y[[1]],main=paste("You have selected", input$Element),new=FALSE)
     })
     
-  }
 })
--- AACA-ESD-Atlas/ui.R
+++ AACA-ESD-Atlas/ui.R
@@ -6,33 +6,31 @@ library(esd)
 shinyUI(fluidPage(
   titlePanel("Winter temperature in the Barents region"),
   
-  fluidRow(
-    column(12,
-      h1("MET KlimaLab"),
-      h2("Climate atlas for the Barents region"),
-      p("This page presents results from empirical-statistical downscaling - also referred to as the abbreviation 'ESD' - described in the paper Benestad et al., ERL-102170.R2 (accepted)."),
-      p("The results presented here repersent large multi-model ensembles from CMIP5 experiment, and include the RCP 2.6, 4.5, and 8.5 emission scenarios."),
-      p("The results have been gridded through kriging and with the elveation as a covariate."),
-      br(),
-      div("This R-shiny app lets you look at the statistics of an ensemble, highlight individual model runs, and pick a time period. You can view the actual values or differences between time or scenarios.", style = "color:blue")
-    )),
+#  fluidRow(
+#    column(12,
+#      h1("MET KlimaLab"),
+#      h2("Climate atlas for the Barents region"),
+#      p("This page presents results from empirical-statistical downscaling - also referred to as the abbreviation 'ESD' - described in the paper Benestad et al., ERL-102170.R2 (accepted)."),
+#      p("The results presented here repersent large multi-model ensembles from CMIP5 experiment, and include the RCP 2.6, 4.5, and 8.5 emission scenarios."),
+#      p("The results have been gridded through kriging and with the elveation as a covariate."),
+#      br(),
+#      div("This R-shiny app lets you look at the statistics of an ensemble, highlight individual model runs, and pick a time period. You can view the actual values or differences between time or scenarios.", style = "color:blue")
+#    )),
    fluidRow(
     column(2,
-           selectInput(h4("Elements"), 
-                        label = "Choose a variable to display",
+           selectInput(h4("Element"), 
+                        label = "Variable",
                         choices = c("Temperature", "Precipitation"),
                         selected = "Temperature")
                               ),
     
     column(2, 
-           checkboxGroupInput("checkGroup", 
-                              label = h4("Season"), 
-                              choices = list("Winter" = 1, 
-                                             "Spring" = 2, 
-                                             "Summer" = 3,
-                                             "Autumn" = 4),
-                              selected = 1)),
-    
+           selectInput(h4("Season"), 
+                       label = "Season",
+                       choices = c("Winter", "Spring","Summer","Autumn"),
+                       selected = "Winter")
+    ),
+   
     column(2, 
            checkboxGroupInput("checkGroup", 
                               label = h4("Scenario"), 
@@ -41,33 +39,38 @@ shinyUI(fluidPage(
                                              "RCP8.5" = 3),
                               selected = 1)),
     column(2,
-           checkboxGroupInput("checkGroup", 
-                              label = h4("Show"), 
-                              choices = list("Actual value" = 1, 
-                                             "Change" = 2, 
-                                             "Scenario difference" = 3),
-                              selected = 1)),
+           selectInput(h4("Aspect"), 
+                       label = "Aspect",
+                       choices = c("actual value", "change","scenario difference"),
+                       selected = "Actual value")
+    ),
     column(2,
-           checkboxGroupInput("checkGroup", 
-                              label = h4("What"), 
-                              choices = list("Ensemble mean" = 1, 
-                                             "95-percentile" = 2, 
-                                             "5-percentile" = 3,
-                                             "Realisation" = 4),
-                              selected = 1))
-  ),
+           selectInput(h4("Statistic"), 
+                       label = "Statistic",
+                       choices = c("Ensemble mean", "95-percentile","5-percentile","Individual model"),
+                       selected = "Ensemble mean"),
+           
+    column(1, 
+          numericInput("Modelrun", 
+                      label = h4("Simulation"), 
+                      value = 1))
+    )
+    ),
+         
   fluidRow(
     
     column(3,
-           sliderInput("range", 
-                       label = "Time slice:",
-                       min = 1900, max = 2100, value = c(1900, 2100))),
-    
-    
-    column(3, 
-           numericInput("Modelrun", 
-                        label = h4("Simulation #"), 
-                        value = 1))
+           sliderInput("lon", 
+                       label = "longitude region:",
+                       min = 0, max = 50, value = c(0, 50))),
+    column(3,
+           sliderInput("lat", 
+                       label = "latitude region:",
+                       min = 65, max = 85, value = c(65, 85))),
+    column(3,
+           dateRangeInput("dates", label = h3("time slice"), start='2090-01-01',end='2099-12-31')),
+    column(3,
+           dateRangeInput("reference", label = h3("baseline", start='1980-01-01',end='2010-21-31')))
   ),
   mainPanel(
     plotOutput("map")
