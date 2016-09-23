a7107c8ba7631afdeb7d0ef7a1e6933f8129da7d
abdelkaderm <abdelkaderm@met.no>
2016-09-23 09:00:16 +0200

    Add station_explorer shiny app

---
+++ station_explorer/server.R
@@ -0,0 +1,199 @@
+library(shiny)
+library(DT)
+# Load the ggplot2 package which provides
+# the 'mpg' dataset.
+library(esd)
+selectrowindex <- 1
+# Define a server for the Shiny app
+shinyServer(function(input, output) {
+  
+  # Filter data based on selections
+  output$table <- DT::renderDataTable({
+    datatable(select.station()[c(2,3,13,10,8,9,4,5,6)],
+              colnames = c('Location','Country','Variable','Source','Start','End','Longitude','Latitude','Altitude'),
+              filter='top',selection = 'single',options = list()) %>% 
+      formatRound(c('longitude','latitude'), digits=2) 
+  })
+  output$text <- renderText({
+    txt <- "This browse tab contains meta data for a list of commonly used weather variables from various freely available global and/or national weather data sets. 
+           The various sources that have been included are : MET Norway archive (eklima.met.no) daily (METNOD) and monthly (METNOM) dataset, 
+    the Global historical climatology network on a daily (GHCND) and monthly (GHCNM) datasets, the European climate data sets (ECAD), the Nordic monthly data sets (NACD) and data
+    from the Nordic Arctic Research Program (NARP)."
+  })
+  
+  output$text1 <- renderText({
+    txt <- 'Description of the weather variables'
+  })
+
+  output$text2 <- renderText({
+    txt <- 'Filter your search based on column names and click on one station from the table'
+  })
+  
+  output$varnames <- DT::renderDataTable({
+    datatable(rbind(c('T2m','Mean temperature'),
+                    c('Tmin','Minimum temperature'),
+                    c('Tmax','Maximum temperature'),
+                    c('Precip','Total Precipitation'),
+                    c('SS','Sunshine'),
+                    c('Tl','Lowest minimum temperature'),
+                    c('Dtl','Day of Tl'),
+                    c('Rh', 'Relative humidity'),
+                    c('Slp','Mean sea level pressure'),
+                    c('Dsc','Number of days with snow cover 50 covered'),
+                    c('Fx','Wind speed'),
+                    c('Fd','Wind direction'),
+                    c('Fg','Wind Gust'),
+                    c('Pmax','Maximum 1 day precipitation'),
+                    c('CC','Cloud cover'),
+                    c('SD','Snow depth'),
+                    c('SF','Snowfall')),
+              colnames = c('Abbreviation','Long name'),options = list(pageLength = 3))
+  })
+  
+  output$plot <- renderPlot({
+    library(esd)
+    #browser()
+    if (length(input$table_rows_selected)>0) {
+      selectedrowindex <- input$table_rows_selected[length(input$table_rows_selected)]
+      selectedrowindex <- as.numeric(selectedrowindex)
+      selectedrow <- (select.station()[selectedrowindex,]) # datasetInput() 
+      #selectedrow
+    } else
+      selectedrowindex <- 1
+    plot(station(select.station()[selectedrowindex,]),new=FALSE)  # datasetInput()
+    #data(Oslo)
+    #plot(Oslo)
+  })
+  
+  output$selectedrow <- DT::renderDataTable({
+    if (length(input$table_rows_selected)>0) {
+      selectedrowindex <- input$table_rows_selected[length(input$table_rows_selected)]
+      selectedrowindex <- as.numeric(selectedrowindex)
+    } else
+      selectedrowindex
+    selectedrow <- (select.station()[selectedrowindex,])
+    selectedrow
+    ##plot(station(select.station()[selectedrowindex,]))  
+  })
+  
+  output$selsta <- DT::renderDataTable({
+    #browser()
+    if (length(input$table_rows_selected)>0) {
+      selectedrowindex <- input$table_rows_selected[length(input$table_rows_selected)]
+      selectedrowindex <- as.numeric(selectedrowindex)
+    } else 
+      selectedrowindex <- 1
+    selectedrow <- (select.station()[selectedrowindex,])
+    selectedrow
+    sel.sta <- select.station()[selectedrowindex,]  
+    DT::datatable(sel.sta,options = list(dom = 't'))
+  })
+  
+  output$rawdata <- DT::renderDataTable({
+    #browser()
+    if (length(input$table_rows_selected)>0) {
+      selectedrowindex <- input$table_rows_selected[length(input$table_rows_selected)]
+      selectedrowindex <- as.numeric(selectedrowindex)
+    } else 
+      selectedrowindex <- 1
+    selectedrow <- (select.station()[selectedrowindex,])
+    selectedrow
+    sel.sta <- station(select.station()[selectedrowindex,])  
+    # browser()
+    DT::datatable(data.frame(Date=index(sel.sta),Value=coredata(sel.sta)),filter='top',options = list(paging = TRUE))
+  })
+  
+  output$map <- renderPlot({
+    library(esd)
+    if (length(input$table_rows_selected)>0) {
+      selectedrowindex <- input$table_rows_selected[length(input$table_rows_selected)]
+      selectedrowindex <- as.numeric(selectedrowindex)
+    } else 
+      selectedrowindex <- 1
+    selectedrow <- (select.station()[selectedrowindex,])
+    selectedrow
+    st <- station(select.station()[selectedrowindex,])
+    map(st,showall=TRUE,xlim=c(-180,180),ylim=c(-90,90),new=FALSE,cex=3)  # datasetInput() # select.station(),subset=
+    points(lon(st),lat(st),cex=1.5,col='red',bg='orange')
+  })
+  
+  # output$plots <- renderPlot({
+  #   
+  #   variable <- table[selectedrowindex,1]
+  #   #write your plot function
+  # })
+  
+  # downloadHandler() takes two arguments, both functions.
+  # The content function is passed a filename as an argument, and
+  #   it should write out data to that filename.
+  
+  #output$plot <- renderPlot({
+  # library(esd)
+  # data(Oslo)
+  # # y <- station(datasetInput())
+  # plot(Oslo,new=FALSE)
+  #})
+  
+  output$downloadMeta <- downloadHandler(
+    
+    # This function returns a string which tells the client
+    # browser what name to use when saving the file.
+    filename = function() {
+      library(esd)
+      if (length(input$table_rows_selected)>0) {
+        selectedrowindex <- input$table_rows_selected[length(input$table_rows_selected)]
+        selectedrowindex <- as.numeric(selectedrowindex)
+      } else
+        selectedrowindex <- 1
+      #browser()
+      param <- ele2param(ele = select.station()[selectedrowindex,7],src=select.station()[selectedrowindex,10])
+      loc <- select.station()[selectedrowindex,2]
+      src <-  select.station()[selectedrowindex,10]
+      cntr <- select.station()[selectedrowindex,3]
+      paste(gsub(' ','-',param[2]),'_',loc,'-',cntr,'_',src,'meta.csv', sep = "")
+    },
+    
+    # This function should write data to a file given to it by
+    # the argument 'file'.
+    content = function(file) {
+      library(esd)
+      if (length(input$table_rows_selected)>0) {
+        selectedrowindex <- input$table_rows_selected[length(input$table_rows_selected)]
+        selectedrowindex <- as.numeric(selectedrowindex)
+      } else
+        selectedrowindex <- 1
+      # Write to a file specified by the 'file' argument
+      write.csv(as.data.frame(select.station()[selectedrowindex,]), file, row.names = FALSE)
+    }
+  )
+  
+  output$downloadData <- downloadHandler(
+    
+    # This function returns a string which tells the client
+    # browser what name to use when saving the file.
+    filename = function() {
+      library(esd)
+      if (length(input$table_rows_selected)>0) {
+        selectedrowindex <- input$table_rows_selected[length(input$table_rows_selected)]
+        selectedrowindex <- as.numeric(selectedrowindex)
+      } else
+        selectedrowindex <- 1
+      param <- ele2param(ele = select.station()[selectedrowindex,7],src=select.station()[selectedrowindex,10])
+      loc <- select.station()[selectedrowindex,2]
+      src <-  select.station()[selectedrowindex,10]
+      cntr <- select.station()[selectedrowindex,3]
+      paste(gsub(' ','-',param[2]),'_',loc,'-',cntr,'_',src,'data.csv', sep = "")
+    },
+    
+    # This function should write data to a file given to it by
+    # the argument 'file'.
+    content = function(file) {
+      # Write to a file specified by the 'file' argument
+      data <- esd::station(select.station()[1,])
+      #save(data,file=file)
+      write.table(data.frame(Date=index(data),Value=coredata(data)), file=file, row.names = FALSE)
+    }
+  )
+  
+})
+
---
+++ station_explorer/ui.R
@@ -0,0 +1,80 @@
+library(shiny)
+library(DT)
+# Load the ggplot2 package which provides
+# the 'mpg' dataset.
+library(esd)
+selsta <- select.station()
+var.names <- c('Mean temperature','Minimum temperature','Maximum temperature',
+               'Precipitation','Sunshine','Lowest minimum temperature','Day of Tl',
+               'Relative humidity','Mean Pressure','Number of days with snow cover 50 covered',
+               'Wind speed','Wind direction','Wind Gust','Maximum 1 day precipitation','Cloud cover',
+               'Snow depth','Snowfall')
+
+# Define the overall UI
+
+navbarPage(title = 'Weather station explorer (Beta version)', 
+           id = 'x0',
+           tabPanel('Browse', 
+                    h4(textOutput('text')),
+                    h3(textOutput('text1')),
+                    dataTableOutput('varnames'),
+                    h3(textOutput('text2')),
+                    DT::dataTableOutput("table")
+                    #plotOutput("plot")
+           ),
+           tabPanel('Plot', 
+                    #textInput('x2', 'Row ID'), 
+                    plotOutput("plot")
+           ),
+           tabPanel('Locate', 
+                    #textInput('x2', 'Row ID'), 
+                    plotOutput("map")#,width = "auto", height = "auto")
+           ),
+           tabPanel('View data', 
+                    #textInput('x2', 'Row ID'), 
+                    DT::dataTableOutput("selsta",width = 'auto'),
+                    DT::dataTableOutput("rawdata",width = 'auto')
+           ),
+           tabPanel('Download', 
+                    #textInput('x2', 'Row ID'), 
+                    downloadLink('downloadMeta', 'Download Meta'),
+                    downloadLink('downloadData', 'Download Data') 
+           )
+)
+
+# shinyUI(
+# fluidPage(
+#   titlePanel("ESD - Station Explorer"),
+#   #sidebarLayout(
+#   #sidebarPanel(
+  #   #    actionButton("newplot", "New plot")
+  #   #),
+  #   # Create a new Row in the UI for selectInputs
+  #   #mianPanel(
+  #     fluidRow(
+  #       column(width=4,
+  #              downloadLink('downloadMeta', 'Download Meta') 
+  #       ),
+  #       column(width=4,
+  #              downloadLink('downloadData', 'Download Data') 
+  #       ),
+  #       plotOutput("map"),
+  #       # # Copy the line below to make a slider range 
+  #       #  column(width=4,
+  #       #         sliderInput("period", label = h3("Temporal coverage"), min = as.integer(1950), 
+  #       #              max = as.integer(2020), step=1, value = as.integer(c(1970, 2010)))
+  #       #  )
+  #    # ),
+  #     # Create a new row for the table.
+  #   #  mainPanel(
+  #       #fluidRow(
+  #         DT::dataTableOutput("table"),
+  #       #)#),
+  #     #mainPanel(
+  #       DT::dataTableOutput("selectedrow"),
+  #       plotOutput("plot")
+  #       
+  #     #)
+  #   )
+  # )
+  #)
