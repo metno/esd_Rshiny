e95e3c451185082abc5bf81778982aac2e1119ac
Rasmus E. Benestad <rasmus.benestad@met.no>
2016-10-11 22:13:30 +0200

    Update server.R

--- AACA-ESD-Atlas/server.R
+++ AACA-ESD-Atlas/server.R
@@ -1,10 +1,17 @@
 library(shiny)
 library(esd)
+
+## Preparations - grid the station data and reduce the size of the data by keeping only
+## the most important PCA modes.
 if (!file.exists('dse.eof.t2m.rcp45.rda')) {
   load("dse.aaca.t2m.rcp45.djf.rda")
+  x <- subset(dse.aaca.t2m.rcp45,pattern=1:3)
+  demo(pca2eof,ask=FALSE)
   Z <- as.eof.dsensemble.pca(dse.aaca.t2m.rcp45,verbose=TRUE)
+  #class(Z) <- c("dsensemble", "eof", "list")
   save(file='dse.eof.t2m.rcp45.rda',Z)
 } else load('dse.eof.t2m.rcp45.rda')
+
 shinyServer(function(input, output) {
   
    output$map <- renderPlot({ 
