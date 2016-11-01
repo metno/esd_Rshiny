7313284e76814f90828a32cd238a76f92c01a090
brasmus <rasmus.benestad@met.no>
2016-11-01 15:31:36 +0100

    Trial-set-up for the AACA Barents Atlas

--- AACA-ESD-Atlas/server.R
+++ AACA-ESD-Atlas/server.R
@@ -3,19 +3,65 @@ library(esd)
 
 ## Preparations - grid the station data and reduce the size of the data by keeping only
 ## the most important PCA modes.
-if (!file.exists('dse.eof.t2m.rcp45.rda')) {
-  load("dse.aaca.t2m.rcp45.djf.rda")
-  x <- subset(dse.aaca.t2m.rcp45,pattern=1:3)
-  demo(pca2eof,ask=FALSE)
-  Z <- as.eof.dsensemble.pca(dse.aaca.t2m.rcp45,verbose=TRUE)
-  #class(Z) <- c("dsensemble", "eof", "list")
-  save(file='dse.eof.t2m.rcp45.rda',Z)
-} else load('dse.eof.t2m.rcp45.rda')
+
+season <- 1
+Z4 <- list()
+load('data/dse.aaca.t2m.rcp45.djf.eof.rda')
+Z4$djf <- Z
+load('data/dse.aaca.t2m.rcp45.mam.eof.rda')
+Z4$mam <- Z
+load('data/dse.aaca.t2m.rcp45.jja.eof.rda')
+Z4$jja <- Z
+load('data/dse.aaca.t2m.rcp45.son.eof.rda')
+Z4$son <- Z
+iloc <- 1
 
 shinyServer(function(input, output) {
   
-   output$map <- renderPlot({ 
-    plot(Y[[1]],main=paste("You have selected", input$Element),new=FALSE)
+  #is <- list(lon=input$lon,lat=input$lat)
+  #it <- as.Date(input$Dates)
+  
+  Season <- reactive({ 
+    switch(input$season,
+           'djf'=1,'mam'=2,'jja'=3,'son'=4)})
+  Im <- reactive({input$model})
+  Lon <- reactive({input$lon})
+  Lat <- reactive({input$lat})
+  #is <- list(lon=Lon(),lat=Lat())
+  Dates <- reactive({
+    seq(min(year(input$dates)),max(year(input$dates)),by=1)
     })
+  Baseline <- reactive({
+    seq(min(year(input$baseline)),max(year(input$baseline)),by=1)
+  })
+  Param <- reactive({input$variable})
+  Rcp <- reactive({input$scenario})
+  Loc <- reactive({input$location})
+  
+  print(names(Z4))
+  gcmnames <<- names(Z4[[season]])[-c(1,2,length(Z4[[season]]))]
+  
+  #print(names(z))
+  
+  #output$map <- renderPlot({plot(rnorm(100),rnorm(100),main='test')})
+  output$map <- renderPlot({ 
+    #esd::map(Z4[[Season()]],it=It(),is=list(lon=Lon(),lat=Lat()),im=Im(),
+    #         main=paste("You have selected", Param()),new=FALSE)},
+    esd::map(Z4[[1]],it=2050,
+             main="You have selected",new=FALSE)},
+    height=function(){800})
+  output$plot <- renderPlot({
+    zz <- Z4[[Season()]]; zz$eof <- NULL;
+    class(zz) <- c('dsensemble','pca','list')
+    z <- as.station(zz)
+    iloc <- (1:length(names(z)))[is.element(names(z),Loc())]
+    plot::plot(z[[iloc]],main=names(z)[iloc],new=FALSE)
+  }, height=function(){800})
+  output$plot.ens <- renderPlot({ 
+    zz <- Z4[[Season()]]; zz$eof <- NULL;
+    class(zz) <- c('dsensemble','pca','list')
+    z <- as.station(zz)
+    plot(subset(z,is=Loc()),im=Im(),main='Ensemble plot',new=FALSE)},
+    height=function(){800})
     
 })
--- AACA-ESD-Atlas/ui.R
+++ AACA-ESD-Atlas/ui.R
@@ -3,12 +3,12 @@ library(esd)
 
 # ui.R
 
-shinyUI(fluidPage(
-  titlePanel("Winter temperature in the Barents region"),
-  
+#shinyUI(fluidPage(
+#  titlePanel("Downscaled temperature in the Barents region"),
+#  
 #  fluidRow(
 #    column(12,
-#      h1("MET KlimaLab"),
+#      h1("MET Klimasandkassen"),
 #      h2("Climate atlas for the Barents region"),
 #      p("This page presents results from empirical-statistical downscaling - also referred to as the abbreviation 'ESD' - described in the paper Benestad et al., ERL-102170.R2 (accepted)."),
 #      p("The results presented here repersent large multi-model ensembles from CMIP5 experiment, and include the RCP 2.6, 4.5, and 8.5 emission scenarios."),
@@ -16,65 +16,94 @@ shinyUI(fluidPage(
 #      br(),
 #      div("This R-shiny app lets you look at the statistics of an ensemble, highlight individual model runs, and pick a time period. You can view the actual values or differences between time or scenarios.", style = "color:blue")
 #    )),
-   fluidRow(
-    column(2,
-           selectInput(h4("Element"), 
-                        label = "Variable",
-                        choices = c("Temperature", "Precipitation"),
-                        selected = "Temperature")
-                              ),
-    
-    column(2, 
-           selectInput(h4("Season"), 
-                       label = "Season",
-                       choices = c("Winter", "Spring","Summer","Autumn"),
-                       selected = "Winter")
-    ),
-   
-    column(2, 
-           checkboxGroupInput("checkGroup", 
-                              label = h4("Scenario"), 
-                              choices = list("RCP4.5" = 1, 
-                                             "RCP2.6" = 2, 
-                                             "RCP8.5" = 3),
-                              selected = 1)),
-    column(2,
-           selectInput(h4("Aspect"), 
-                       label = "Aspect",
-                       choices = c("actual value", "change","scenario difference"),
-                       selected = "Actual value")
-    ),
-    column(2,
-           selectInput(h4("Statistic"), 
-                       label = "Statistic",
-                       choices = c("Ensemble mean", "95-percentile","5-percentile","Individual model"),
-                       selected = "Ensemble mean"),
-           
-    column(1, 
-          numericInput("Modelrun", 
-                      label = h4("Simulation"), 
-                      value = 1))
-    )
-    ),
-         
-  fluidRow(
-    
-    column(3,
-           sliderInput("lon", 
-                       label = "longitude region:",
-                       min = 0, max = 50, value = c(0, 50))),
-    column(3,
-           sliderInput("lat", 
-                       label = "latitude region:",
-                       min = 65, max = 85, value = c(65, 85))),
-    column(3,
-           dateRangeInput("dates", label = h3("time slice"), start='2090-01-01',end='2099-12-31')),
-    column(3,
-           dateRangeInput("reference", label = h3("baseline", start='1980-01-01',end='2010-21-31')))
-  ),
-  mainPanel(
-    plotOutput("map")
-  ))
+#   fluidRow(
+
+
+#    column(1, 
+#           checkboxGroupInput("Modelrun", 
+#                             label = h4("Simulation"), 
+#                             choices = names(Z[[1]])[-c(1,2)],
+#                             selected=1:108))
+#    ),
+#         
+#  fluidRow(
+#    
+
+
+#  ),
+#  mainPanel(
+#    plotOutput("map")
+#  ))
+#)
+
+# Choices for drop-downs
+# Show a tabset that includes a plot, summary, and
+# table view of the generated distribution
+
+
+navbarPage("AACA Atlas",
+    tabPanel("Ensemble statistics", 
+             plotOutput("map"),
+             column(3,
+                   sliderInput("lon", 
+                                    label = "Longitudes:",
+                                    min = 0, max = 50, value = c(0, 50))),
+             column(3,
+                        sliderInput("lat", 
+                                    label = "Latitudes:",
+                                    min = 65, max = 85, value = c(65, 85))),
+             column(3,
+                        dateRangeInput("dates", label = h3("time slice"), start='2090-01-01',end='2099-12-31'))
+             ),
+    tabPanel("Locations", 
+             column(2,
+                    selectInput(h4("Location"), 
+                                label = "location",
+                                choices = names(z),
+                                selected = "longyearbyen")
+             ),
+             plotOutput("plot")),
+    tabPanel("Settings", 
+             column(2,
+                    selectInput(h4("Element"), 
+                                            label = "Variable",
+                                            choices = c("Temperature", "Precipitation"),
+                                            selected = "Temperature")
+                    ),
+                        
+             column(2, 
+                    selectInput(h4("Season"), 
+                                           label = "Season",
+                                           choices = c("Winter", "Spring","Summer","Autumn"),
+                                           selected = "Winter")
+                    ),
+                      
+            column(2, 
+                   selectInput(h4("Scenario"), 
+                                            label = "Scenario", 
+                                            choices = c("RCP4.5","RCP2.6", "RCP8.5"),
+                                            selected = "RCP4.5")),
+            column(3,
+                       dateRangeInput(h4("Baseline"), label = "baseline", start='1980-01-01',end='2010-21-31')),
+            column(2,
+                       selectInput(h4("Aspect"), 
+                                   label = "Aspect",
+                                   choices = c("actual value", "change","scenario difference"),
+                                   selected = "Actual value")
+            ),
+                column(2,
+                       selectInput(h4("Statistic"), 
+                                   label = "Statistic",
+                                   choices = c("Ensemble mean", "95-percentile","5-percentile","Individual model",
+                                               "Ensemble spread","Model difference"),
+                                   selected = "Ensemble mean")
+            ),
+            column(4, 
+                   checkboxGroupInput(h4("Modelrun"), 
+                                      label = "Model", 
+                                      choices = gcmnames,
+                                      selected=gcmnames)),
+  
+             
+            tableOutput("table"))
 )
-
-
